b1a6ab3e4c6c96060b774805ebc9799a09fce3e9
Brian Litwin <brian.n.litwin@gmail.com>
2018-12-29 21:20:14 -0500

    reverse linked list in c

--- reverse-linked-list/solution.c
+++ reverse-linked-list/solution.c
@@ -6,43 +6,58 @@
  * };
  */
 
+ struct ListNode* reverseList(struct ListNode* head) {
+   struct ListNode* node = head;
+   struct ListNode* prev = NULL;
 
+   while (node) {
+     struct ListNode* temp = node->next;
+     node->next = prev;
+     prev = node;
+     node = temp;
+   }
+
+   return prev;
+ }
+
+/*
 int countNodes(struct ListNode *head);
 
 struct ListNode* reverseList(struct ListNode* head) {
-    
+
     if (head == NULL) return NULL;
-    
+
     int count = countNodes(head);
-    
-    struct ListNode *arr[count + 1]; 
-    
-    struct ListNode *next = head; 
-    count = 0; 
-    arr[0] = next; 
-    
+
+    struct ListNode *arr[count + 1];
+
+    struct ListNode *next = head;
+    count = 0;
+    arr[0] = next;
+
     while ((next = next->next) != NULL) {
         count += 1;
-        arr[count] = next; 
+        arr[count] = next;
     }
-    
-    int i; 
+
+    int i;
     arr[0]->next = NULL;
-    
+
     for (i=1;i<=count;i++) {
         arr[i]->next = arr[i-1];
     }
-    
+
     return arr[count];
 }
 
 int countNodes(struct ListNode *head) {
     int count = 0;
     struct ListNode *next = head;
-    
+
     while ((next = next->next) != NULL) {
         count += 1;
     }
-    
-    return count; 
+
+    return count;
 }
+*/
--- reverse-linked-list/solution.js
+++ reverse-linked-list/solution.js
@@ -9,6 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+ 
+var reverseList = function(head) {
+    var node = head; 
+    var prev = null; 
+    
+    while (node != null) {
+        // make a copy of node.next 
+        var temp = node.next 
+        node.next = prev
+        prev = node 
+        node = temp 
+    }
+    
+    return prev 
+};
 
 
 
