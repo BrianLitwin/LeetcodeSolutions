3803ff503b321d386f44dbf93294e95b67449bd2
Brian Litwin <brian.n.litwin@gmail.com>
2019-01-01 20:29:44 -0500

    solution

---
+++ binary-tree-preorder-traversal/solution.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    var array = [];
+    var n = root; 
+    var answer = [];
+    
+    while (n != null || array.length > 0) {
+        if (n != null) {
+            array.push(n);
+            answer.unshift(n.val);
+            n = n.right; 
+            
+        } else {
+            const last = array.pop();
+            n = last.left 
+        }
+        
+    }
+    
+    return answer;
+};
