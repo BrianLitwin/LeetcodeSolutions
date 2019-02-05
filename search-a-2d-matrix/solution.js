3771a9c248239f2ca4f0531b41c3065518457716
Brian Litwin <brian.n.litwin@gmail.com>
2019-02-05 09:39:31 -0500

    Update solution.js

--- search-a-2d-matrix/solution.js
+++ search-a-2d-matrix/solution.js
@@ -4,46 +4,30 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    if (matrix.length === 0 ) {
-        return false; 
-    }
-    
-    var len = matrix[0].length
-    
-    var bs = (row) => {
-        return binarySearch(row, target, 0, len - 1)
-    }
-    
-    for (var i in matrix) {
-        if (matrix[i][len - 1] >= target) {
-            return bs(matrix[i]); 
+    for (const row of matrix) {
+        if (row[row.length - 1] < target) { 
+            continue 
         } 
-    }
+        
+        if (binarySearch(row, 0, row.length - 1, target)) { 
+            return true 
+        }
+    }   
+    
     return false 
 };
 
-function binarySearch(nums, targ, start, end) {
-    if (start > end ) {
-        return false 
-    }
-    
-    const mid = parseInt((start + end) / 2)
-    
-    var searchLeft = () => {
-        return binarySearch(nums, targ, start, mid - 1)
-    }
-    
-    var searchRight = () => {
-        return binarySearch(nums, targ, mid + 1, end)
-    }
-    
-    if (nums[mid] === targ) {
-        return true; 
-    } else if (nums[mid] > targ) {
-        return searchLeft()
-    } else if (nums[mid] < targ) {
-        return searchRight()
+
+function binarySearch(arr, start, end, targ) {
+    const mid = Math.round((start + end) / 2 )
+
+    if (start > end) {
+        return false     
+    } else if (arr[mid] == targ) {
+        return true 
+    } else if (arr[mid] < targ) {
+        return binarySearch(arr, mid + 1, end, targ)
     } else {
-        return false 
+        return binarySearch(arr, start, mid - 1, targ)
     }
 }
