6ce5286a56f40112854dd7b225506935d246f49c
Brian Litwin <brian.n.litwin@gmail.com>
2019-02-26 14:42:06 -0500

    Update and rename generate-parentheses to generate-parentheses.js

---
+++ generate-parentheses.js
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/generate-parentheses/
+// CTCI 8.9 
+
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var generateParenthesis = function(n) {
+    var answer = [];
+    var cur = ""
+    getParens(n, 0, 0, [], answer)
+    return answer 
+};
+
+function getParens(max, l, r, arr, answer) {
+  if (r < l && r < max) {
+    var array = arr.slice()
+    array.push(")")
+    getParens(max, l, r + 1, array, answer)
+  }
+
+  if (l < max) {
+    var array = arr.slice()
+    array.push("(")
+    getParens(max, l + 1, r, array, answer)
+  }
+
+  if (l === max && r === max) {
+    answer.push(arr.join(""))
+  }
+}
