39d77ed2512979a504fd4cefd3e833743a513cc5
Brian Litwin <brian.n.litwin@gmail.com>
2019-05-18 12:13:19 -0400

    Update solution.cljs

--- codesignal/arcade/4-adjacentElementsProduct/solution.cljs
+++ codesignal/arcade/4-adjacentElementsProduct/solution.cljs
@@ -2,9 +2,9 @@
     (for [i (range (- (count vec) 1))] [(vec i) (vec (+ i 1))])
 )
 
-(defn get-sum [pair] (* (first pair) (second pair)))
+(defn get-prod [pair] (* (first pair) (second pair)))
 
 (defn adjacentElementsProduct [inputArray] 
-    (reduce max (vec (map get-sum (makepairs inputArray))))
+    (reduce max (vec (map get-prod (makepairs inputArray))))
 )
 
